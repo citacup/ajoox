bd833dd2f2817cbc5ca86fc199a3af84a50a3e9b
Cita Audia Ahlunaza <cita.audia.ahlunaza@gmail.com>
2016-07-14 20:57:46 +0900

    test commit edit artist name

---
+++ app/src/main/java/com/ictidn/cita/ajoox/ListActivity.java
@@ -0,0 +1,52 @@
+package com.ictidn.cita.ajoox;
+
+/**
+ * Created by ajou on 7/14/2016.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ListActivity extends Activity {
+
+    private ListView lv;
+    // Listview Adapter
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_layout);
+
+        TextView title = (TextView) findViewById(R.id.toolbar_title);
+        Typeface faceDea = Typeface.createFromAsset(getAssets(), "fonts/deathstar.otf");
+        title.setTypeface(faceDea);
+
+        // Listview Data
+        String products[] = {"Backstreet Boys",
+                "Carly Rae Jepsene",
+                "Eminem",
+                "Girls Generation",
+                "Calvin Harris",
+                "Kesha" };
+
+        lv = (ListView) findViewById(R.id.list_view);
+
+        // Adding items to listview
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.product_name, products);
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        Intent i = new Intent(ListActivity.this, ScrollingActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
+
