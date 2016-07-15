e5732ba1046b6549d8b70b921359fc50c79a6664
Mohammad Syahid Wildan <mohammad.syahid.wildan@gmail.com>
2016-07-16 00:02:46 +0900

    merge commit add search genre

--- app/src/main/java/com/ictidn/cita/ajoox/GridActivity.java
+++ app/src/main/java/com/ictidn/cita/ajoox/GridActivity.java
@@ -45,11 +45,11 @@ public class GridActivity extends Activity {
                 break;
             case "genre":
                 typeText.setText("All Genre");
-                gridview.setAdapter(new ImageAdapter(this, data.getArtist(""), R.drawable.genre));
+                gridview.setAdapter(new ImageAdapter(this, data.getGenre(""), R.drawable.sound_icon));
                 break;
             case "country":
                 typeText.setText("All Country");
-                gridview.setAdapter(new ImageAdapter(this, data.getArtist(""), R.drawable.country));
+                gridview.setAdapter(new ImageAdapter(this, data.getArtist(""), R.drawable.sound_icon));
                 break;
         }
 
